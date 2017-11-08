fb77e4c5f7457ec93cea006197c8bd7c6fff16ea
hualf1995 <32714950+hualf1995@users.noreply.github.com>
2017-11-08 12:01:34 -0800

    Update Binary Tree Preorder Traversal.java

--- Binary tree - Divide Conquer/Binary Tree Preorder Traversal.java
+++ Binary tree - Divide Conquer/Binary Tree Preorder Traversal.java
@@ -35,20 +35,21 @@ public class Solution {
 
 //Version 1: Traverse(recursion)
 public class Solution {
-    public ArrayList<Integer> preorderTraversal(TreeNode root) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        traverse(root, result);
+    /*
+     * @param root: A Tree
+     * @return: Preorder in ArrayList which contains node values.
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        preorderTraversalR(root,result);
         return result;
     }
-    // 把root为跟的preorder加入result里面
-    private void traverse(TreeNode root, ArrayList<Integer> result) {
-        if (root == null) {
-            return;
-        }
-
+    // put the preorder of root into the result
+    private void preorderTraversalR(TreeNode root, List result){
+        if(root == null)  return;
         result.add(root.val);
-        traverse(root.left, result);
-        traverse(root.right, result);
+        preorderTraversalR(root.left, result);
+        preorderTraversalR(root.right, result);
     }
 }
 
